b11e1d11dccb3a42fd18e9ff8daed7ea6d8d198a
claxico <hblackmo@andrew.cmu.edu>
2024-03-21 09:44:58 -0400

    first commit

---
+++ Java Fish.js
@@ -0,0 +1,382 @@
+class Card {
+
+    static ranks = ["", "", "two", "three", "four", "five", "six", "seven", 
+    "eight", "nine", "ten", "jack", "queen", "king", "ace"]
+
+    static rankSymbols = ["", "", "2", "3", "4", "5", "6", "7", "8", "9",
+    "10", "J", "Q", "K", "A"]
+
+    static suits = ["spades", "diamonds", "hearts", "clubs"]
+    static suitSymbols = ["♠️", "♦️", "♥️", "♣️"]
+
+    constructor(rank, suit) {
+        this.rank = rank
+        this.suit = suit
+    }
+
+    get symbol() {
+        const rankString = Card.rankSymbols[this.rank]
+        const suitString = Card.suitSymbols[this.suit]
+
+        return rankString + suitString
+    }
+
+    get name() {
+        const rankString = Card.ranks[this.rank]
+        const suitString = Card.suits[this.suit]
+
+        return rankString + " of " + suitString
+    }
+
+    isInSameSetAs(other) {
+        if (this.rank == 8) {
+            if (other.rank == 8) return true
+            else return false
+        } else if (this.suit == other.suit) {
+            if ((this.rank < 8) && (other.rank < 8) ||
+                (this.rank > 8) && (other.rank > 8)) {
+                return true
+            }
+        } else {
+            return false
+        }
+    }
+
+    get set() {
+        let set = []
+
+        for (const card of deck) {
+            if (card.isInSameSetAs(this)) {
+                set.push(card)
+            }
+        }
+
+        return set
+
+    }
+
+    equals(other) {
+        return this.rank == other.rank && this.suit == other.suit
+    }
+
+    toJSON() {
+		return {
+			rank: this.rank,
+			suit: this.suit,
+			symbol: this.symbol
+		};
+	}
+}
+
+function makeNewDeck() {
+    let deck = []
+
+    for (let rank = 2; rank < 15; rank ++) {
+        for (let suit = 0; suit < 4; suit ++)  {
+            const c = new Card(rank, suit)
+            deck.push(c)
+        }
+    }
+
+    return deck
+}
+
+const CardStatus = {
+    inHand: "inHand",
+    maybe: "maybe",
+    unknown: "unknown",
+    unlikely: "unlikely",
+    notInHand: "notInHand"
+}
+
+const deck = makeNewDeck()
+    
+class Player {
+    points = 0;
+    cardStatusDictionary = {};
+    opponents = [];
+    hand = [];
+    partner;
+    isComputer;
+
+    constructor(name) {
+        this.name = name
+        
+        for (const card of deck) {
+            this.cardStatusDictionary[card.symbol]= CardStatus.unknown
+        }
+    }
+
+    getPartners(players) {
+        for (let i = 0; i < players.length; i++) {
+            players[i].partner = players[(i+2) % 4]
+            players[i].opponents = []
+            players[i].opponents.push(players[(i+1) % 4])
+            players[i].opponents.push(players[(i+3) % 4])
+        }
+    }
+
+    addCard(card) {
+        for (let i = 0; i < this.hand.length; i++) {
+            if (this.hand[i].suit == card.suit) {
+                if (this.hand[i].rank > card.rank) {
+                    this.hand.splice(i, 0, card)
+                    return
+                }
+            } else if (this.hand[i].suit > card.suit) {
+                this.hand.splice(i, 0, card)
+                return
+            }
+        }
+
+        this.hand.splice(this.hand.length, 0, card)
+    }
+
+    hasCard(card) {
+        for (let i = 0; i < this.hand.length; i++) {
+            if (this.hand[i].equals(card)) {
+                return true
+            }
+        }
+
+        return false
+    }
+
+    removeCard(card) {
+        for (let i = 0; i < this.hand.length; i++) {
+            if (this.hand[i].equals(card)) {
+                return this.hand.splice(i, 1)
+            }
+        }
+    }
+
+    canAskForCard(card) {
+        for (const c of this.hand) {
+            if (card.isInSameSetAs(c)) return true
+        }
+        return false
+    }
+
+    get allowedAskingCards() {
+        let cards = []
+        for (const c of deck) {
+            if (this.canAskForCard(c)) {
+                cards.push(c)
+            }
+        }
+
+        return cards
+    }
+
+    get sortedHand() {
+
+        let sortedHand = Array.from(this.hand)
+
+        sortedHand.sort( function(a, b) {
+            if (a.suit == b.suit) return a.rank - b.rank
+            else return a.suit - b.suit;
+        });
+
+       return sortedHand
+    }
+
+    // Returns true if legal ask
+    askForCard(target, card, players) {
+    
+        if (!this.canAskForCard(card)) {
+            console.log("Invalid card!!")
+            return null
+        }
+
+        for (const c of card.set) {
+            if (this.cardStatusDictionary[c.symbol] == CardStatus.unknown) {
+                this.cardStatusDictionary[c.symbol] = CardStatus.maybe
+            }
+        }
+
+        if (target.hasCard(card)) {
+            target.removeCard(card);
+            this.addCard(card);
+
+
+            // No one else has it
+            for (const player of players) {
+                player.cardStatusDictionary[card.symbol] = CardStatus.notInHand
+            }
+
+            // The asking player has the card
+            this.cardStatusDictionary[card.symbol] = CardStatus.inHand
+
+            return true
+        } else {
+            target.cardStatusDictionary[card.symbol] = CardStatus.notInHand
+            this.cardStatusDictionary[card.symbol] = CardStatus.unlikely
+            return false
+        }
+    }
+
+    declareSet(card, players) {
+        let goodDeclaration = true
+
+        // Go through all cards of deck
+        for (const c of card.set) {
+            if (this.hasCard(c)) {
+                this.removeCard(c)
+            } else if (this.partner.hasCard(c)) {
+                this.partner.removeCard(c)
+            } else {
+                goodDeclaration = false
+            }
+        }
+
+        if (goodDeclaration) {
+            this.points += 1
+        } else {
+            this.opponents[0].points += 1
+            for (const c of card.set) {
+                for (let player of players) player.removeCard(c)
+            }
+        }
+        
+        return
+
+    }
+
+    getComputerCard(target) {
+
+        // Possible choices — cards you can ask for, and that you don't have
+        const askingCards = this.allowedAskingCards.filter(x => !this.hasCard(x))
+
+        // Ask for all cards you know they have
+        for (const card of askingCards) {
+
+            if (target.cardStatusDictionary[card.symbol] == CardStatus.inHand) {
+                return card
+            }
+        }
+
+
+        const choice = Math.floor(Math.random() * 2)
+
+        // Ask for cards they might have
+        if (choice == 0) {
+            for (const card of askingCards) {
+                if (target.cardStatusDictionary[card.symbol] == CardStatus.maybe) {
+                    return card
+                }
+            }
+        }
+
+
+        // Ask for random cards
+        for (const card of askingCards) {
+            if (target.cardStatusDictionary[card.symbol] == CardStatus.unknown) {
+                return card
+            }
+        }
+        
+    }
+    
+    getComputerTarget() {
+        if (this.opponents[0].hand.size == 0) {
+            return this.opponents[1]
+        } else if (this.opponents[1].hand.size == 0) {
+            return this.opponents[0]
+        } else {
+            return this.opponents[Math.floor(2 * Math.random())]
+        }
+    }
+
+    tryComputerDeclare(players) {
+
+        let i = 0;
+        while (i < this.sortedHand.length) {
+            const card = this.sortedHand[i]
+
+            let shouldDeclare = true
+            for (const c of card.set) {
+
+                if (!(this.hasCard(c) || this.partner.cardStatusDictionary[c.symbol] == CardStatus.inHand ||
+                (this.opponents[0].cardStatusDictionary[c.symbol] == CardStatus.notInHand && 
+                    this.opponents[1].cardStatusDictionary[c.symbol] == CardStatus.notInHand))) {
+                    
+                    shouldDeclare = false
+                    break
+                }
+            }
+
+            if (shouldDeclare) {
+                this.declareSet(card, players)
+            } else {
+                i += 1
+            }
+        }
+    }
+}
+
+function gameIsOver(players) {
+    for (const player of players) {
+        if (player.hand.size != 0) {
+            return false
+        }
+    }
+    return true
+}
+
+function setUpPlayers(nameList, isComputerList) {
+    let player1 = new Player(nameList[0])
+    let player2 = new Player(nameList[1])
+    let player3 = new Player(nameList[2])
+    let player4 = new Player(nameList[3])
+
+    let players = [player1, player2, player3, player4]
+
+    for (let i = 0; i < players.length; i++) {
+        players[i].isComputer = isComputerList[i]
+        players[i].getPartners(players)
+    }
+
+    return players
+}
+
+function dealCards(players) {
+
+    let currentIndex = deck.length 
+    let randomIndex;
+  
+    while (currentIndex != 0) {
+  
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      [deck[currentIndex], deck[randomIndex]] = [
+        deck[randomIndex], deck[currentIndex]];
+    }
+
+    for (let i = 0; i < 52; i ++) {
+        players[i%4].addCard(deck[i])
+    }
+}
+
+function findNextPlayer(goal) {
+
+    for (const player of [goal, goal.partner, goal.opponents[0], goal.opponents[1]]) {
+        if (player.hand.size != 0) {
+            return player
+        }
+    }
+
+}
+
+// runGame()
+
+
+module.exports = {
+    Card,
+    makeNewDeck,
+    Player,
+    gameIsOver,
+    setUpPlayers,
+    dealCards,
+    findNextPlayer,
+}
---
+++ index.js
@@ -0,0 +1,311 @@
+const jf = require('./Java Fish.js');
+let express = require('express');
+
+// Web socket setup
+const http = require("http");
+const { client } = require("websocket");
+const app = require("express")();
+app.get("/", (req,res)=> res.sendFile(__dirname + "/index.html"))
+app.use(express.static(__dirname + '/'));
+
+// Host on port 9090
+// Listen on port 9091, so to text go to localhost:9091
+app.listen(9091, ()=>console.log("Listening on http port 9091"))
+const websocketServer = require("websocket").server
+const httpServer = http.createServer();
+httpServer.listen(9090, () => console.log("Listening.. on 9090"))
+
+const clients = {};
+const players = {};
+const games = {};
+
+// Global variables
+const MAX_PLAYERS = 4;
+const TOTAL_PLAYERS = 4;
+const fs = require("fs");
+var available_game_names = fs.readFileSync('./fishnames.txt').toString().split("\n");
+
+// Helper function to avoid player circles
+const replacerFunc = () => {
+    // const visited = new WeakSet();
+    return (key, value) => {
+      if (value instanceof jf.Player) { 
+
+        if (typeof(value.partner) === "object") {
+            value.partner = value.partner.name
+            value.opponents[0] = value.opponents[0].name
+            value.opponents[1] = value.opponents[1].name
+        }
+        
+      }
+      return value;
+    };
+};
+
+const reviver = function(key, value) {
+
+    console.log(key, value)
+
+    if (value.hand !== undefined) { // It's a player
+
+        let player = new jf.Player(value.name)
+        player.hand = value.hand
+        player.cardStatusD
+        player.points = value.points
+        player.cardStatusDictionary = value.cardStatusDictionary
+        player.isComputer = value.isComputer
+
+        return player
+        
+    } else if (value.rank !== undefined) { // It's a card
+        let card = new jf.Card(value.rank, value.suit)
+        return card
+
+    }
+
+    return value;
+}
+
+// Server
+const wsServer = new websocketServer({
+    "httpServer": httpServer
+})
+
+function createGame(result) {
+    const clientId = result.clientId;
+    const gameId = getGameId()
+
+    const newGame = {
+        "id": gameId,
+        "clients": [],
+        "players": [],
+        "logs": [],
+        "sets": [],
+        "currentPlayer": undefined,
+        "starterClientId": result.clientId,
+        "owner": ""
+    }
+
+    // Add game id and metadata to game dictionary
+    games[gameId] = newGame
+
+    // Data to return back to game HTML
+    const payload = {
+        "method": "gameCreated",
+        "game": newGame
+    }
+
+    clients[clientId].connection.send(JSON.stringify(payload))
+}
+
+function joinGame(result, connection) {
+
+    const clientId = result.clientId;
+    const name = result.name;
+    const gameId = result.gameId;
+    const game = games[gameId];
+
+    // Check if game id is valid
+    if (game === undefined) {
+        const payload = {
+            "method": "alert",
+            "message": "Sorry, this game doesn't exist!"
+        }
+        connection.send(JSON.stringify(payload));
+        return;
+    } else if (game.clients.length >= MAX_PLAYERS) {
+        const payload = {
+            "method": "alert",
+            "message": "Sorry, this game is maxed out on players!"
+        }
+        connection.send(JSON.stringify(payload));
+        return;
+    }
+    
+    // Add client to game 
+    game.clients.push({
+        "clientId": clientId,
+        "name": name,
+    })
+
+    // Inform each client of the game status
+    const payload = {
+        "method": "playerJoined",
+        "game": game
+    }
+
+    for (const client of game.clients) {
+        clients[client.clientId].connection.send(JSON.stringify(payload, replacerFunc()))
+    }
+}
+
+function startGame(game) {
+    const nameList = game.clients.map(client => client.name)
+
+    let isAIList = nameList.map(function (name) { return false; })
+    let computerNames = ["Conan", "Ana", "Hasan"]
+    
+    while (nameList.length < 4) {
+        const name = getComputerName(computerNames)
+        nameList.push(name)
+        isAIList.push(true)
+    }
+
+
+    game.players = jf.setUpPlayers(nameList, isAIList)
+
+    game.currentPlayer = game.players[0]
+
+    jf.dealCards(game.players)
+
+    console.log("players:",game.players)
+
+    const payload = {
+        "method": "gameStarted",
+        "game": game
+    }
+    
+    for (const client of game.clients) {
+        clients[client.clientId].connection.send(JSON.stringify(payload, replacerFunc()))
+    }
+}
+
+function updateGame(game) {
+
+    const payload = {
+        "method": "update",
+        "game": game
+    }
+    
+    for (const client of game.clients) {
+        clients[client.clientId].connection.send(JSON.stringify(payload, replacerFunc()))
+    }
+}
+
+function askForCard(game, askerName, targetName, card) {
+    asker = game.players.find(player => player.name == askerName)
+    target = game.players.find(player => player.name == targetName);
+
+    const result = asker.askForCard(target, card, game.players)
+
+    let log = {
+        "askerName": asker.name,
+        "cardSymbol": card.symbol,
+        "targetName": target.name,
+        "result": undefined
+    }
+
+    if (result == null) {
+        game.currentPlayer = jf.findNextPlayer(game.currentPlayer)
+        return
+    }
+
+    if (result == false) {
+        log.result = "no"
+        game.currentPlayer = jf.findNextPlayer(target)
+    } else if (result == true) {
+        log.result = "yes"
+        game.currentPlayer = jf.findNextPlayer(game.currentPlayer)
+    }
+
+    game.logs.push(log)
+    updateGame(game)
+
+}
+
+function doComputerTurn(game) {
+
+    const target = game.currentPlayer.getComputerTarget(game.players)
+
+    const card = game.currentPlayer.getComputerCard(target)
+    askForCard(game, game.currentPlayer.name, target.name, card)
+
+}
+
+function declareSet(game, declarerName, card) {
+
+    declarer = game.players.find(player => player.name == declarerName)
+    declarer.declareSet(card, game.players)
+
+    game.sets.push(card)
+
+    updateGame(game)
+   
+}
+
+
+wsServer.on("request", request => {
+    // Accept client request to open or close connection
+    const connection = request.accept(null, request.origin);
+    connection.on("open", () => console.log("opened connection"))
+    connection.on("close", () => console.log("closed connection"))
+
+    // Recieve instructions via JSON message
+    connection.on("message", message => {
+
+
+        console.log(message.utf8Data)
+
+
+        const result = JSON.parse(message.utf8Data, reviver)
+
+        if (result.gameId !== undefined) {
+            const game = games[result.gameId]
+            for (const player of game.players) {
+                player.getPartners(game.players)
+            }
+        }
+
+        switch (result.method) {
+            case "createGame":
+                createGame(result)
+                break;
+            case "join":
+                joinGame(result, connection)
+                break;
+            case "startGame":
+                startGame(games[result.gameId])
+                break;
+            case "askForCard":
+                askForCard(games[result.gameId], result.asker, result.targetName, result.card)
+                break;
+            case "doComputerTurn":
+                doComputerTurn(games[result.gameId])
+                break;
+            case "declareSet":
+                declareSet(games[result.gameId], result.declarerName, result.card)
+                break;
+        }
+
+    })
+
+    // Upon connection to server, generate a new clientId and send back to client in JSON payload
+    const clientId = guid();
+    clients[clientId] = {
+        "connection":  connection,
+    };
+    const payload = {
+        "method": "connect",
+        "clientId": clientId
+    };
+
+    connection.send(JSON.stringify(payload));
+})
+
+// Random game id generation helpers
+function S4() {
+    return (((1+Math.random())*0x10000)|0).toString(16).substring(1); 
+}
+
+function guid() {
+    return (S4() + S4() + "-" + S4() + "-4" + S4().substr(0,3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
+}
+
+function getGameId() {
+    return available_game_names.splice(Math.floor(Math.random() * available_game_names.length), 1)[0];
+}
+
+function getComputerName(computerNames) {
+    return computerNames.splice(Math.floor(Math.random() * computerNames.length), 1)[0];
+
+}
